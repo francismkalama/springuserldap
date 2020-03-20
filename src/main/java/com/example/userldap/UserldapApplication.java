e4acc124889dff1d0220b8df9fad92f2c66ebacd
francis mkalama <francis.mkalama@gmail.com>
2020-03-21 01:43:19 +0300

    user LDAP authentication

---
+++ src/main/java/com/example/userldap/UserldapApplication.java
@@ -0,0 +1,13 @@
+package com.example.userldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserldapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserldapApplication.class, args);
+	}
+
+}
