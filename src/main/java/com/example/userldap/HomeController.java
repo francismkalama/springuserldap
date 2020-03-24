644bb05d7daee1ec62efd8c4d403a202d502978a
francis mkalama <francis.mkalama@gmail.com>
2020-03-24 17:30:33 +0300

    complete user LDAP project

---
+++ src/main/java/com/example/userldap/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.userldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController{
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>Welcome to home page</h1>");
+    }
+}
--- src/test/java/com/example/userldap/UserldapApplicationTests.java
+++ src/test/java/com/example/userldap/UserldapApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.userldap;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserldapApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.userldap;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserldapApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
